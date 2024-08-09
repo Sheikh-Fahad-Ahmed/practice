6dfc08d1bd325d57099823150730d7426ac9f898
Fahad Ahmed <fahad838@gmail.com>
2024-08-09 11:03:45 +0530

    Add div with: border,bg color, p , h1

--- javascript.js
+++ javascript.js
@@ -19,3 +19,19 @@ heading.textContent = "This is a blue h3";
 heading.setAttribute("style", "color:blue");
 
 container.appendChild(heading);
+
+const secondContent = document.createElement("div");
+secondContent.classList.add("secondContent");
+secondContent.setAttribute("style", "border: 1px solid black;background-color:pink");
+
+const secondHeading = document.createElement("h1");
+secondHeading.textContent ="Im in a div";
+
+const secondPara = document.createElement("p");
+secondPara.textContent = "Me Too!";
+
+secondContent.appendChild(secondHeading);
+secondContent.appendChild(secondPara);
+
+container.appendChild(secondContent);
+
