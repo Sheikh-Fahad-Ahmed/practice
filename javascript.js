bbaa9be736dfbff0a47903de13d76190bc59f02c
Fahad Ahmed <fahad838@gmail.com>
2024-08-09 10:26:10 +0530

    Add p tag with red text using js

--- javascript.js
+++ javascript.js
@@ -5,3 +5,10 @@ content.classList.add("content");
 content.textContent = "This is a text content";
 
 container.appendChild(content);
+
+const para = document.createElement("p");
+para.classList.add("paragraph");
+para.textContent = "This is red text";
+para.setAttribute("style","color:red");
+
+container.appendChild(para);
